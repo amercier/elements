48ecf0e56d679854a47928181031c4f77c7d5e6e
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-07-18 18:16:50 -0700

    Added basic tests for constructor

--- test/constructor.spec.js
+++ test/constructor.spec.js
@@ -1,10 +1,56 @@
 import Elements from '../src/elements';
+import slice from '../src/helpers/slice';
 
 describe('new Elements()', () => {
 
   it('doesn\'t fail', function() {
     expect(() => {
+      new Elements();
+    }).not.to.throw();
+  });
+});
+
+describe('new Elements(document)', () => {
+
+  it('doesn\'t fail', function() {
+    expect(() => {
       new Elements(document);
     }).not.to.throw();
+  });
+});
+
+describe('new Elements(NodeList)', () => {
+
+  it('doesn\'t fail', function() {
+    expect(() => {
+      new Elements(document.querySelectorAll('*'));
+    }).not.to.throw();
+  });
+});
+
+describe('new Elements(HTMLElement[])', () => {
+
+  it('doesn\'t fail', function() {
+    expect(() => {
+      new Elements(slice(document.querySelectorAll('*')));
+    }).not.to.throw();
+  });
+});
+
+describe('new Elements(String)', () => {
+
+  it('doesn\'t fail', function() {
+    expect(() => {
+      new Elements('*');
+    }).not.to.throw();
+  });
+});
+
+describe('new Elements({})', () => {
+
+  it('fails', function() {
+    expect(() => {
+      new Elements({});
+    }).to.throw('Expected input to be a Node or an array-like object, got [object Object]');
   });
 });
