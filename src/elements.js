19d6878dd1b745929051b3d6dd99072aeabb635a
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-07-26 17:49:34 -0700

    Added internals/toArray

--- src/elements.js
+++ src/elements.js
@@ -2,9 +2,8 @@ import matches from './helpers/matches';
 import slice from './helpers/slice';
 import union from './helpers/union';
 import flatten from 'lodash/internal/baseFlatten';
-import uniq from 'lodash/internal/baseUniq';
 
-const toString = Object.prototype.toString;
+import toArray from './internals/toArray';
 
 export default class Elements {
 
@@ -14,21 +13,7 @@ export default class Elements {
    * @param {Node|Node[]|NodeList|null} input Input DOM node, or array of nodes.
    */
   constructor(input) {
-    if (!input) {
-      this.elements = [];
-    }
-    else if (input instanceof Elements) {
-      this.elements = slice(input.elements);
-    }
-    else if (input instanceof Node) {
-      this.elements = [input];
-    }
-    else if (input.length !== undefined) {
-      this.elements = uniq(slice(input));
-    }
-    else {
-      throw new Error('Expected input to be a Node or an array-like object, got ' + toString.call(input));
-    }
+    this.elements = toArray(input instanceof Elements ? input.elements : input);
   }
 
   find(selector) {
